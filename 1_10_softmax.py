872d4ac732098bb839847f22c75b8c45f71c045b
Bob <bobf@supermicro.com.tw>
2018-04-18 08:16:47 +0800

    10. Quiz: Softmax

---
+++ 1_10_softmax.py
@@ -0,0 +1,22 @@
+"""Softmax."""
+
+scores = [3.0, 1.0, 0.2]
+
+import numpy as np
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    #pass  # TODO: Compute and return softmax(x)
+    x_exp = np.exp(x)
+    return x_exp / np.sum(x_exp, axis=0)
+
+
+print(softmax(scores))
+
+# Plot softmax curves
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
+
